824aff8af93652a2ed88f00b264c33db8fbed68c
Kevin Langer <34527204+kelanger@users.noreply.github.com>
2018-02-06 11:23:20 -0800

    Refactored and debugged

--- app.js
+++ app.js
@@ -12,45 +12,54 @@ const phrases = ["Cow jumped over the moon",
 					"But her emails",
 					"I can not do that Dave"
 					]; 
-let winningPhrase;
 let missed = 0;
 let letterFound;
 
+
+// Create random phrase
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
 
-function getRandomPhraseArray (arr){
+function getRandomPhraseArray (arr) {
 	let phrasesNum = getRandomInt(phrases.length);
-	return phrases[phrasesNum];
+	return phrases[phrasesNum].split("");
 }
 
+
+// Initiate and reset game
 function playGame() {
 	missed = 0;
 	phraseId.removeChild(phraseId.firstElementChild);
 	phraseId.appendChild(document.createElement("ul"));
-	winningPhrase = getRandomPhraseArray(phrases).split("");
+	let randomWord = getRandomPhraseArray(phrases);
+	addPhraseToDisplay(randomWord);
 	let chosen = document.getElementsByClassName("chosen");
-	console.log(chosen);
 	while (chosen.length){
+		chosen[0].disabled = false;
 		chosen[0].className = "";
 	}
 	for (i = 0; i < scoreboard.length; i ++) {
 		scoreboard[i].innerHTML = '<img src="images/liveHeart.png" height="35px" width="35px">';
 	}
-	for (i = 0; i < winningPhrase.length; i++) {
+	overlay.style.display = "none";
+}
+
+function addPhraseToDisplay(arr) {
+	for (i = 0; i < arr.length; i++) {
 		let li = document.createElement("li");
-		li.innerHTML = winningPhrase[i];
-		if (winningPhrase[i] === " ") {
+		li.innerHTML = arr[i];
+		if (arr[i] === " ") {
 			li.className = "space";
 		} else {
 			li.className = "letter";
 		}
 		phraseId.firstElementChild.appendChild(li);
 	}
-	overlay.style.display = "none";
 }
 
+
+// Check user selection and phrase
 function checkLetter(button) {
 	let chosen = document.getElementsByClassName("letter");
 	let wasLetterFound = false;
@@ -67,23 +76,25 @@ function checkLetter(button) {
 	}
 }
 
+
+// Check for win or lose conditions
 function checkWin () {
 	const title = document.getElementsByClassName("title")[0];
 	const show = document.getElementsByClassName("show");
 	const letter = document.getElementsByClassName("letter");
 	if (missed >= 5) {
-		console.log("LOSER");
 		overlay.style.display = "flex";
 		title.textContent = "Sorry, but you lost.";
 		overlay.className = "lose";
 	} else if (letter.length === show.length) {
-		console.log("WINER");
 		overlay.style.display = "flex";
 		title.textContent = "Congratulations, you won!";
 		overlay.className = "win";
 	}
 }
 
+
+// Event Listeners
 startButton.addEventListener("click", () => {
 	playGame();
 	});
@@ -92,11 +103,10 @@ qwerty.addEventListener("click", (e) => {
 	if (e.target.tagName === 'BUTTON') {
 		e.target.className = "chosen";
 		letterFound = checkLetter(e.target);
-		console.log(letterFound);
+		e.target.disabled = true;
 		if (!letterFound) {
 			scoreboard[missed].innerHTML = '<img src="images/lostHeart.png" height="35px" width="35px">';
 			missed ++;
-			console.log("No Letter Found");
 		}
 		checkWin();
 	}
